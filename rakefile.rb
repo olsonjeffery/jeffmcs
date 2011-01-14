e665b386a90dd0f81e813b2831fd1d0af442ea29
Jeff Olson <olson.jeffery@gmail.com>
2011-01-14 08:53:25 -0800

    more stuffs

--- rakefile.rb
+++ rakefile.rb
@@ -1,95 +1,74 @@
 task :default => [:build]
 
 @root = Dir.pwd
-@serverPath = "#{@root}/server"
+@serverTemplate = "#{@root}/server_template"
+@cbServerPath = "#{@root}/server_cb"
+@vanillaServerPath = "#{@root}/server_vanilla"
 @worldPicPath = "#{@root}/worldpics"
-@hmodPath = "#{@root}/src/hmod"
 @bukkitPath = "#{@root}/src/bukkit"
 @craftbukkitPath = "#{@root}/src/craftbukkit"
-
-task :gitbar => [:setupserverdir, :gitserver, :runhmodserver]
-task :gitserver => [:gitbuild, :bininstall]
-
-task :runhmodserver do
-  Dir.chdir @serverPath
-  sh './server_nogui_hmod.sh'
-end
-task :runserver do
-  Dir.chdir @serverPath
-  sh './server_nogui.sh'
-end
-
-task :bininstall do
-  Dir.chdir @serverPath
-  sh "mv Minecraft_Mod.jar Minecraft_Mod.jar.old"
-  sh "mv #{@root}/bin/Minecraft_Mod.jar ./"
-  sh "mv minecraft_server.jar minecraft_server.jar.old"
-  sh "mv #{@root}/bin/minecraft_server.jar ./"
-  Dir.chdir @root
-end
-
-task :gitbuild => [:fetch_minecraft_server] do
-  Dir.chdir "#{@hmodPath}/src"
-  sh 'rm -f *.class *.jar'
-  sh "javac *.java -cp #{@root}/bin/minecraft_server.jar && jar cvfm Minecraft_Mod.jar #{@root}/Manifest.mf *.class"
-  sh "cp Minecraft_Mod.jar #{@root}/bin"
-  Dir.chdir @root
-end
-
-task :fetch_minecraft_server do
-  sh "wget http://minecraft.net/download/minecraft_server.jar -O #{@root}/bin/minecraft_server.jar"
-end
 
 def add_remote(user, repo)
   sh "git remote add #{user} git://github.com/#{user}/#{repo}.git"
   sh "git fetch #{user}"
 end
 
-task :reclone do
-  sh "rm -Rf #{@hmodPath}"
-  defaultRepo = "Minecraft-Server-Mod"
-  sh "git clone git://github.com/traitor/#{defaultRepo}.git #{@hmodPath}"
-  Dir.chdir @hmodPath
-  add_remote "Dinnerbone", defaultRepo
-  add_remote "angelsl", "hMod"
-  add_remote "durron597", defaultRepo
-  Dir.chdir @root
+def setupDirFromTemplate(serverDir, templateDir, worldDir)
+  sh "rm -Rf #{serverDir}"
+  sh "cp -Rf #{templateDir} #{serverDir}"
+  sh "ln -sf #{worldDir} #{serverDir}/world"
 end
 
-task :newpic do
-  sh "../mcmap/mcmap -from -20 -20 -to 20 20 -file webapp/public/worldpics/world.png ./world"
-  sh "convert -size 500x500 webapp/public/worldpics/world.png -resize 500x500 webapp/public/worldpics/world_preview.png"
-  sh "../mcmap/mcmap -night -from -20 -20 -to 20 20 -file webapp/public/worldpics/world_night.png ./world"
-  sh "convert -size 500x500 webapp/public/worldpics/world_night.png -resize 500x500 webapp/public/worldpics/world_night_preview.png"
+# rebuild, setup and run craftbukkit
+task :docb => [:initcb, :runcb]
+
+# download, setup and run the vanilla server 
+task :dovanilla => [:initvanilla, :runvanilla]
+
+# vanilla server setup
+task :initvanilla => [:fetch_minecraft_server] do
+  setupDirFromTemplate @vanillaServerPath, @serverTemplate, "#{@root}/world"
+  sh "cp #{@root}/bin/minecraft_server.jar #{@vanillaServerPath}/"
 end
 
-# new hottness for bukkit-based server. hopefully migrate to
-# this soon.
+task :runvanilla do
+  Dir.chdir @vanillaServerPath
+  sh "./server_nogui_vanilla.sh"
+end
+
+task :fetch_minecraft_server do
+  sh "wget http://minecraft.net/download/minecraft_server.jar -O #{@root}/bin/minecraft_server.jar"
+end
+
+# craftbukkit build/setup
+
+task :initcb => [:setupservercb, :installcb]
+
+task :runcb do
+  Dir.chdir @cbServerPath
+  sh './server_nogui_cb.sh'
+end
+
 task :bb do
   Dir.chdir @bukkitPath
-  sh 'mvn clean compile package'
-  sh "mvn install:install-file -DgroupId=org.bukkit -DartifactId=bukkit -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar -Dfile=#{@bukkitPath}/target/bukkit-0.0.1-SNAPSHOT.jar"
+  sh 'mvn clean install'
   Dir.chdir @root
 end
 
 task :cbb => [:bb] do
   Dir.chdir @craftbukkitPath
-  sh 'mvn clean compile package'
+  sh 'mvn clean package'
   sh "cp target/craftbukkit*jar #{@root}/bin/craftbukkit.jar"
   Dir.chdir @root
 end
 
-task :cbinstall => [:cbb] do
-  sh "cp -Rf #{@root}/bin/craftbukkit.jar #{@serverPath}/"
+task :installcb => [:cbb] do
+  sh "cp -Rf #{@root}/bin/craftbukkit.jar #{@cbServerPath}/"
 end
 
-task :setupserverdir do
-  sh "rm -Rf #{@serverPath}"
-  sh "cp -Rf #{@root}/server-skeleton #{@serverPath}"
-  sh "ln -sf #{@root}/world #{@serverPath}/world"
+task :setupservercb do
+  setupDirFromTemplate @cbServerPath, @serverTemplate, "#{@root}/world"
 end
-
-task :initcbserver => [:setupserverdir, :cbinstall]
 
 task :gitbukkit do
   sh "rm -Rf #{@bukkitPath}"
@@ -104,4 +83,12 @@ task :bukkitpull do
   Dir.chdir @craftbukkitPath
   sh 'git pull'
   Dir.chdir @root
+end
+
+# worldpic server management
+task :newpic do
+  sh "../mcmap/mcmap -from -20 -20 -to 20 20 -file webapp/public/worldpics/world.png ./world"
+  sh "convert -size 500x500 webapp/public/worldpics/world.png -resize 500x500 webapp/public/worldpics/world_preview.png"
+  sh "../mcmap/mcmap -night -from -20 -20 -to 20 20 -file webapp/public/worldpics/world_night.png ./world"
+  sh "convert -size 500x500 webapp/public/worldpics/world_night.png -resize 500x500 webapp/public/worldpics/world_night_preview.png"
 end
---
+++ server_template/server_nogui_vanilla.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -Xms1024M -Xmx1024M -jar minecraft_server.jar nogui
